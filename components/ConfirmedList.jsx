376dcaf3d71c54d3780e571e21dab8e045521d03
siteoficial <gabriellima5494@gmail.com>
2025-08-19 17:22:55 -0300

    Update ConfirmedList.jsx

--- components/ConfirmedList.jsx
+++ components/ConfirmedList.jsx
@@ -1,1 +1,19 @@
+import React from "react";
+
+export default function ConfirmedList({ confirmados }) {
+  return (
+    <div>
+      <h3>Confirmados ✅</h3>
+      {confirmados.length === 0 ? (
+        <p>Ninguém confirmou ainda</p>
+      ) : (
+        <ul>
+          {confirmados.map((item, index) => (
+            <li key={index}>{item.nome}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
 
