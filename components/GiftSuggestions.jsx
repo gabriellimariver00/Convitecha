b115158c88b8572d4f3fbd6cf3137a37d420573c
siteoficial <gabriellima5494@gmail.com>
2025-08-19 17:23:33 -0300

    Update GiftSuggestions.jsx

--- components/GiftSuggestions.jsx
+++ components/GiftSuggestions.jsx
@@ -1,1 +1,15 @@
+import React from "react";
+
+export default function Gifts() {
+  return (
+    <div>
+      <h3>Lista de Presentes 🎁</h3>
+      <ul>
+        <li>Fraldas</li>
+        <li>Roupinhas</li>
+        <li>Lenços Umedecidos</li>
+      </ul>
+    </div>
+  );
+}
 
