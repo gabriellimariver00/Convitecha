ebfba6620651fd4ca4b0c0587521552c1c7670bc
siteoficial <gabriellima5494@gmail.com>
2025-08-19 17:23:10 -0300

    Update DirectionsButton.jsx

--- components/DirectionsButton.jsx
+++ components/DirectionsButton.jsx
@@ -1,1 +1,14 @@
+import React from "react";
+
+export default function DirectionsButton() {
+  const handleClick = () => {
+    window.open("https://maps.google.com", "_blank");
+  };
+
+  return (
+    <button onClick={handleClick}>
+      Ver Local no Mapa 📍
+    </button>
+  );
+}
 
