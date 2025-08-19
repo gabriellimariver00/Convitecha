db289512a4a2341def1c7ff04bf7a5087f88f537
siteoficial <gabriellima5494@gmail.com>
2025-08-19 17:19:32 -0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,19 +1,24 @@
 import React, { useState } from "react";
-import WelcomeScreen from "./components/WelcomeScreen.jsx";
-import InvitationDetails from "./components/InvitationDetails.jsx";
+import WelcomeScreen from "./components/WelcomeScreen";
+import InvitationDetails from "./components/InvitationDetails";
+import ConfirmButton from "./components/ConfirmButton";
+import ConfirmedList from "./components/ConfirmedList";
+import DirectionsButton from "./components/DirectionsButton";
+import GiftSuggestions from "./components/GiftSuggestions";
 
-const App = () => {
-  const [currentScreen, setCurrentScreen] = useState("welcome");
+function App() {
+  const [confirmed, setConfirmed] = useState(false);
 
   return (
-    <>
-      {currentScreen === "welcome" ? (
-        <WelcomeScreen onContinue={() => setCurrentScreen("details")} />
-      ) : (
-        <InvitationDetails onBack={() => setCurrentScreen("welcome")} />
-      )}
-    </>
+    <div className="container">
+      {!confirmed && <WelcomeScreen />}
+      <InvitationDetails />
+      <ConfirmButton onConfirm={() => setConfirmed(true)} />
+      {confirmed && <ConfirmedList />}
+      <DirectionsButton />
+      <GiftSuggestions />
+    </div>
   );
-};
+}
 
 export default App;
