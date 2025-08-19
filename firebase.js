37ccecb235807acbf26d51c75f7beafd444fb09a
siteoficial <gabriellima5494@gmail.com>
2025-08-19 17:21:43 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,24 +1,19 @@
-// Importando funções do Firebase
+// Importando SDK do Firebase
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, push, onValue } from "firebase/database";
-import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
 
-// Configuração do seu Firebase
+// Configurações do seu Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBvE4UhFOapllHbXxwxhW5o_y9dgsZUEtg",
   authDomain: "convite-cha-revelacao-511a2.firebaseapp.com",
   databaseURL: "https://convite-cha-revelacao-511a2-default-rtdb.firebaseio.com",
   projectId: "convite-cha-revelacao-511a2",
-  storageBucket: "convite-cha-revelacao-511a2.appspot.com",
+  storageBucket: "convite-cha-revelacao-511a2.firebasestorage.app",
   messagingSenderId: "60150164299",
   appId: "1:60150164299:web:65c2fadb6024bce78556ce",
   measurementId: "G-LQ8DZK3GNP"
 };
 
-// Inicializando o Firebase
+// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const database = getDatabase(app);
-
-// Exportando para usar nos componentes
-export { database, ref, push, onValue };
+export const db = getDatabase(app);
