878207ed844bf7f0dcd9e33d11f159eb8c053bd4
siteoficial <gabriellima5494@gmail.com>
2025-08-19 17:35:42 -0300

    Update Suggestions.jsx

--- components/Suggestions.jsx
+++ components/Suggestions.jsx
@@ -1,1 +1,14 @@
+import React from "react";
 
+export default function Suggestions() {
+  return (
+    <div style={{ marginTop: "30px" }}>
+      <h3>Sugestões de presente:</h3>
+      <ul>
+        <li>Fralda RN PMG</li>
+        <li>Qualquer presente unissex</li>
+      </ul>
+      <p>🎁 Sua presença é o nosso presente mais especial!</p>
+    </div>
+  );
+}
